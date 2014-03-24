382289354ee5e14ce69a2eddca0d7b0798900a60
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2014-03-24 22:12:58 +0000

    Add initial test view

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function(req, res){
-    res.send('Hello World')
+    res.render('index')
 })
 
 var server = app.listen(3000, function() {
