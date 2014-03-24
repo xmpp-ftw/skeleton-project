db2c35a179c591090af1df9f73854afc331fdc76
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2014-03-24 22:03:18 +0000

    Hande a websocket connection/disconnection

--- index.js
+++ index.js
@@ -8,3 +8,26 @@ app.get('/', function(req, res){
 var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port)
 })
+
+var options = {
+    transformer: 'socket.io',
+    parser: 'JSON',
+    transports: [
+        'websocket',
+        'htmlfile',
+        'xhr-polling',
+        'jsonp-polling'
+    ]
+}
+
+var primus = new Primus(server, options)
+primus.use('emitter', Emitter)
+primus.save(__dirname + '/public/scripts/primus.js')
+
+primus.on('connection', function(socket) {
+    console.log('Websocket connection made')
+})
+
+primus.on('disconnection', function(socket) {
+    console.log('Client disconnected')
+})
